54ffb449c7de611ba90338953420b9194f7ce4f0
sohEEEEE1226 <dg2025224@danggok.hs.kr>
2026-06-17 09:51:32 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="청소년 건강 분석",
+    page_icon="🌙",
+    layout="wide"
+)
+
+st.title("🌙 청소년 건강 & 생활시간 분석")
+
+st.markdown("""
+### 프로젝트 소개
+
+본 웹앱은
+
+- 청소년 수면시간 변화
+- 건강인지율 변화
+- 생활시간 변화
+
+를 분석하는 데이터 시각화 프로젝트입니다.
+
+👈 왼쪽 메뉴에서 원하는 분석 페이지를 선택하세요.
+""")
